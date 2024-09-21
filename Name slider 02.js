030e95115aecd3271ef8e84809d560c68051acaf
Susanta Samanta <160142120+SusantaSamanta@users.noreply.github.com>
2024-09-22 01:54:42 +0530

    Last letter size increase

--- Name slider 02.js
+++ Name slider 02.js
@@ -1,6 +1,6 @@
 const nameContainer = document.getElementById("name_container")
 
-let length, string = 'Hello World';
+let length, string = 'SUSANTA SAMANTA';
 length = string.length;
 let time = 600, index = 0;
   
@@ -13,9 +13,17 @@ const loop = (string) => {
   if(t_length == length){
     index = -1;
   }
-  nameContainer.textContent = text;
-  console.log(text);
-  setTimeout(loop, 150, string)
+  
+  let  fist_lett = text.slice(0,index-1);
+  console.log(fist_lett)
+  let last_lett = text.slice(index-1,index);
+  console.log(last_lett)
+  let final_text = `${fist_lett}<span class="${"big_text"}">${last_lett}</span>`;
+  
+  nameContainer.innerHTML = final_text;
+  //console.log(final_text);
+  setTimeout(loop, 1000, string)
+  
 }
 
 setTimeout(() => {
