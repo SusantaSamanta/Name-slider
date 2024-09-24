b7732719d781ad88a8109ed49b9531237b58e731
Susanta Samanta <160142120+SusantaSamanta@users.noreply.github.com>
2024-09-24 23:55:33 +0530

    Last letter size fix

--- Name slider 02.js
+++ Name slider 02.js
@@ -1,6 +1,6 @@
 const nameContainer = document.getElementById("name_container")
 
-let length, string = 'SUSANTA SAMANTA ';
+let length, string = 'SUSANTA SAMANTA* ';
 length = string.length;
 let time = 600, index = 0;
   
@@ -18,20 +18,38 @@ const loop = (string) => {
   
   let last_line = `<span class="${"last_line"}">${"|"}</span>`;
   let fist_lett = text.slice(0,index-1);
-  console.log(fist_lett)
-  let last_lett = text.slice(index-1,index)+last_line;
-  console.log(last_lett)
+ // console.log(fist_lett)
+  
+  
+  let last_lett = 0;
+  if(t_length <= length){
+    if(text[length-2] == '*' ){
+      console.log(t_length)
+      console.log("ok " + text)
+      
+      last_lett = last_line;
+    }
+    else{
+      
+      last_lett = text.slice(index-1,index)+last_line;
+    }
+  }
+  
+  
+  
+ // let last_lett = text.slice(index-1,index)+last_line;
+ // console.log(last_lett)
   let final_text = `${fist_lett}<span class="${"big_text"}">${last_lett}</span>`;
 
-  nameContainer.style.opacity = "1";
+  //nameContainer.style.opacity = "1";
   //nameContainer.setPropertiesValue
   //nameContainer.classList.add('opacity');
  // .style="opacity:1";
   
   
   
-  nameContainer.innerHTML = final_text;
-  //console.log(final_text);
+ nameContainer.innerHTML = final_text;
+  console.log(final_text);
   //}
   setTimeout(loop, 500, string)
 }
@@ -48,5 +66,25 @@ setTimeout(() => {
 let s='susanta'
 let su=s.slice(0,7)
 //console.log(su)
+
+
+
+let txt = "Susanta8"
+
+len = txt.length
+i=len
+if(len == i){
+  //console.log("ok")
+}
+if(len == i){
+  if(i == len){
+    test = 1;
+  }
+  else{
+    test = 2;
+  }
+}
+//console.log(test)
+
 
 
